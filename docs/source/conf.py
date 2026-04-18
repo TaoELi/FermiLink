3a2cc8e0bedfcad903e291f73813a0e57bb2f4ea
Tao E. Li <t.e.li@outlook.com>
2026-04-18 14:50:29 -0400

    improved documentation website rendering

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -96,7 +96,7 @@ html_theme_options = {
     "sidebar_hide_name": False,
     "top_of_page_button": "edit",
     "navigation_with_keys": False,
-    "light_logo": "img/mark.svg",
+    "light_logo": "img/mark-light.svg",
     "dark_logo": "img/mark.svg",
     "light_css_variables": {
         "color-brand-primary": "#1264a3",
